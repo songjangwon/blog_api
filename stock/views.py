c8819b23bad0b006ac5764acf6950474e90d2e07
songjangwon <jwswong124@ezcaretech.com>
2019-12-24 15:44:17 +0900

    계좌정보 얻기 성공

--- stock/views.py
+++ stock/views.py
@@ -36,18 +36,29 @@ class GroupViewSet(viewsets.ModelViewSet):
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("PyStock")
-        self.setGeometry(300, 300, 300, 150)
 
+        # Kiwoom Login
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom.dynamicCall("CommConnect()")
 
+        # OpenAPI+ Event
+        self.kiwoom.OnEventConnect.connect(self.event_connect)
+
+        self.setWindowTitle("계좌 정보")
+        self.setGeometry(300, 300, 300, 150)
+
+        btn1 = QPushButton("계좌 얻기", self)
+        btn1.move(190, 20)
+        btn1.clicked.connect(self.btn1_clicked)
+
         self.text_edit = QTextEdit(self)
         self.text_edit.setGeometry(10, 60, 280, 80)
-        self.text_edit.setEnabled(False)
 
-        self.kiwoom.OnEventConnect.connect(self.event_connect)
+    def btn1_clicked(self):
+        account_num = self.kiwoom.dynamicCall("GetLoginInfo(QString)", ["ACCNO"])
+        self.text_edit.append("계좌번호: " + account_num.rstrip(';'))
 
     def event_connect(self, err_code):
         if err_code == 0:
             self.text_edit.append("로그인 성공")
+

9cc9180aa25885b93fc1842f94c9459297e05acf
songjangwon <jwswong124@ezcaretech.com>
2019-12-24 16:14:02 +0900

    db연결

--- blog/settings.py
+++ blog/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movies',
+        'USER': 'song',
+        'PASSWORD': 'jwsong124',
+        'HOST': '127.0.0.1', # Or an IP Address that your DB is hosted on
+        'PORT': '3307',
+    },
 }
 
 

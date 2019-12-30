db239730c6ef45e59d00ce5c8b13c32f307f4b43
songjangwon <jwswong124@ezcaretech.com>
2019-12-30 17:49:59 +0900

    1230

--- stock/pykiwoom/kiwoom/Kiwoom.py
+++ stock/pykiwoom/kiwoom/Kiwoom.py
@@ -9,6 +9,9 @@ from rest_framework.decorators import action
 from PyQt5.QAxContainer import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QEventLoop
+from pandas import DataFrame
+from datetime import datetime
+import time
 from rest_framework import status
 import os, sys
 
@@ -19,6 +22,39 @@ class Kiwoom(QAxWidget):
         super().__init__()
         self._create_kiwoom_instance()
         self._set_signal_slots()
+        # Loop 변수
+        # 비동기 방식으로 동작되는 이벤트를 동기화(순서대로 동작) 시킬 때
+        self.login_loop = None
+        self.request_loop = None
+        self.order_loop = None
+        self.condition_loop = None
+
+        # 서버구분
+        self.server_gubun = None
+
+        # 조건식
+        self.condition = None
+
+        # 에러
+        self.error = None
+
+        # 주문번호
+        self.order_no = ""
+
+        # 조회
+        self.inquiry = 0
+
+        # 서버에서 받은 메시지
+        self.msg = ""
+
+        # 예수금 d+2
+        self.data_opw00001 = 0
+
+        # 보유종목 정보
+        self.data_opw00018 = {'account_evaluation': [], 'stocks': []}
+
+
+
 
     def _create_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -26,6 +62,14 @@ class Kiwoom(QAxWidget):
 
     def _set_signal_slots(self):
         self.OnEventConnect.connect(self._event_connect)
+        self.OnReceiveTrData.connect(self.on_receive_tr_data)
+        # self.OnReceiveChejanData.connect(self._receive_chejan_data)
+        # self.OnReceiveChejanData.connect(self.on_receive_chejan_data)
+        # self.OnReceiveRealData.connect(self.receive_real_data)
+        # self.OnReceiveMsg.connect(self.receive_msg)
+        # self.OnReceiveConditionVer.connect(self.receive_condition_ver)
+        # self.OnReceiveTrCondition.connect(self.receive_tr_condition)
+        # self.OnReceiveRealCondition.connect(self.receive_real_condition)
 
 
     def comm_connect(self):
@@ -55,3 +99,70 @@ class Kiwoom(QAxWidget):
         account_num = self.dynamicCall("GetLoginInfo(QString)", ["ACCNO"])
         account_num = account_num.rstrip(';')
         return account_num
+
+    def set_input_value(self, id, value):
+        self.dynamicCall("SetInputValue(QString, QString)", id, value)
+
+
+    def comm_rq_data(self, rqname, trcode, next, screen_no):
+        print("!!!!!!!!!!!!!!")
+        self.dynamicCall("CommRqData(QString, QString, int, QString)", rqname, trcode, next, screen_no)
+        print("@@@@@@@@@@@@@")
+        self.tr_event_loop = QEventLoop()
+        self.tr_event_loop.exec_()
+
+    def _comm_get_data(self, code, real_type, field_name, index, item_name):
+        ret = self.dynamicCall("CommGetData(QString, QString, QString, int, QString)", code,
+                               real_type, field_name, index, item_name)
+        return ret.strip()
+    def _get_repeat_cnt(self, trcode, rqname):
+        ret = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
+        return ret
+
+    def on_receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
+        if next == '2':
+            self.remained_data = True
+        else:
+            self.remained_data = False
+
+        if rqname == "opt10081_req":
+            self._opt10081(rqname, trcode)
+        elif rqname == "opw00001_req":
+            self._opw00001(rqname, trcode)
+        elif rqname == "opw00018_req":
+            self._opw00018(rqname, trcode)
+
+        try:
+            self.tr_event_loop.exit()
+        except AttributeError:
+            pass
+
+    def _opw00018(self, rqname, trcode):
+        total_purchase_price = self._comm_get_data(trcode, "", rqname, 0, "총매입금액")
+        total_eval_price = self._comm_get_data(trcode, "", rqname, 0, "총평가금액")
+        total_eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, 0, "총평가손익금액")
+        total_earning_rate = self._comm_get_data(trcode, "", rqname, 0, "총수익률(%)")
+        estimated_deposit = self._comm_get_data(trcode, "", rqname, 0, "추정예탁자산")
+
+        print(Kiwoom.change_format(total_purchase_price))
+        print(Kiwoom.change_format(total_eval_price))
+        print(Kiwoom.change_format(total_eval_profit_loss_price))
+        print(Kiwoom.change_format(total_earning_rate))
+        print(Kiwoom.change_format(estimated_deposit))
+
+    def _opw00001(self, rqname, trcode):
+        self.d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
+        print(self.d2_deposit)
+
+    def send_order(self, rqname, screen_no, acc_no, order_type, code, quantity, price, hoga, order_no):
+        self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+                         [rqname, screen_no, acc_no, order_type, code, quantity, price, hoga, order_no])
+
+    def get_chejan_data(self, fid):
+        print(fid)
+        ret = self.dynamicCall("GetChejanData(int)", fid)
+        return ret
+
+    def test(self):
+        self.set_input_value("계좌번호", "8127732711")
+        self.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
--- stock/views.py
+++ stock/views.py
@@ -30,10 +30,45 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['get'])
     def get_account_list(self, request, pk):
-
-
         account_num = self.kiwoom.get_account_list()
         return Response(account_num, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['get'])
+    def test(self, request, pk):
+        # # 예수금상세현황요청
+        # self.kiwoom.set_input_value("계좌번호", "8127732711")
+        # self.kiwoom.set_input_value("비밀번호", "9978")
+        # print("예수금상세현황요청1")
+        # self.kiwoom.comm_rq_data("예수금상세현황요청", "opw00001", 0, "2000")
+        # print("예수금상세현황요청2")
+        #
+        # # 계좌평가잔고내역요청 - opw00018 은 한번에 20개의 종목정보를 반환
+        # self.kiwoom.set_input_value("계좌번호", "8127732711")
+        # self.kiwoom.set_input_value("비밀번호", "9978")
+        # print("계좌평가잔고내역요청")
+        # self.kiwoom.comm_rq_data("계좌평가잔고내역요청", "opw00018", 0, "2000")
+        # print("계좌평가잔고내역요청2")
+        # item_count = len(self.kiwoom.data_opw00018['stocks'])
+
+        self.kiwoom.set_input_value("계좌번호", "8127732711")
+        self.kiwoom.set_input_value("비밀번호", "9978")
+        self.kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
+        print(self.kiwoom.d2_deposit)
+        return Response({"das": "das"}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['get'])
+    def test2(self, request, pk):
+        self.kiwoom.set_input_value("계좌번호", "8127732711")
+        self.kiwoom.set_input_value("비밀번호", "9978")
+        self.kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
+        print(self.kiwoom.d2_deposit)
+        return Response({"das":"das"}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['get'])
+    def test3(self, request, pk):
+        self.kiwoom.test()
+        return Response({"das":"das"}, status=status.HTTP_200_OK)
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
@@ -42,40 +77,4 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-class Kiwoom(QAxWidget):
-    def __init__(self):
-        super().__init__()
-        self._create_kiwoom_instance()
-        self._set_signal_slots()
-
-    def _create_kiwoom_instance(self):
-        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
-
-
-    def _set_signal_slots(self):
-        self.OnEventConnect.connect(self._event_connect)
-
-
-    def comm_connect(self):
-
-        self.dynamicCall("CommConnect()")
-
-        self.login_event_loop = QEventLoop()
-
-        self.login_event_loop.exec_()
-
-
-
-    def _event_connect(self, err_code):
-        if err_code == 0:
-            print("connected")
-        else:
-            print("disconnected")
-
-        self.login_event_loop.exit()
-
-    def get_code_list_by_market(self, market):
-        code_list = self.dynamicCall("GetCodeListByMarket(QString)", market)
-        code_list = code_list.split(';')
-        return code_list[:-1]
 
